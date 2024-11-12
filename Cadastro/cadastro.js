5691fa7e7f83689ac0218c8d3a13874b4ea62e4a
Leonardo Cardoso <leo-2408@hotmail.com>
2024-11-12 19:12:30 -0300

    estoquefinalizado

---
+++ Cadastro/cadastro.js
@@ -0,0 +1,126 @@
+class Produto {
+
+    constructor() {
+        this.id = 1;
+        this.arrayProdutos = [];
+        this.editId = null;
+    }
+
+    salvar() {
+        let produto = this.lerDados();
+
+        if(this.validaCampos(produto)) {
+            if (this.editId == null) {
+                this.adicionar(produto);
+            } else {
+                this.atualizar(this.editId, produto);
+            }
+        }
+
+        this.listaTabela();
+        this.cancelar();
+    }
+
+    listaTabela() {
+        let tbody = document.getElementById('tbody');
+        tbody.innerText = '';
+
+        for(let i = 0; i < this.arrayProdutos.length; i++) {
+            let tr = tbody.insertRow();
+
+            let td_id = tr.insertCell();
+            let td_produto = tr.insertCell();
+            let td_preco = tr.insertCell();
+            let td_acoes = tr.insertCell();
+
+            td_id.innerText = this.arrayProdutos[i].id;
+            td_produto.innerText = this.arrayProdutos[i].nomeProduto;
+            td_preco.innerText = this.arrayProdutos[i].preco.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' });
+
+            td_id.classList.add('center');
+
+            let imgEdit = document.createElement('img');
+            imgEdit.src = '/Content/editar.png';
+            imgEdit.setAttribute("onclick", "produto.editar("+ this.arrayProdutos[i].id +")");
+
+            let imgDelete = document.createElement('img');
+            imgDelete.src = '/Content/deletar.png';
+            imgDelete.setAttribute("onclick", "produto.deletar("+ this.arrayProdutos[i].id +")");
+
+            td_acoes.appendChild(imgEdit);
+            td_acoes.appendChild(imgDelete);
+
+        }
+    }
+
+    adicionar(produto) {
+        this.arrayProdutos.push(produto);
+        this.id++;
+    }
+
+    lerDados() {
+        let produto = {}
+
+        produto.id = this.id;
+        produto.nomeProduto = document.getElementById('produto').value;
+        produto.preco = parseFloat(document.getElementById('preco').value);
+
+        return produto;
+    }
+
+    validaCampos(produto) {
+        let msg = '';
+
+        if(produto.nomeProduto == '') {
+            msg += 'Informe o nome do produto \n';
+        }
+
+        if(produto.preco == '' || isNaN(produto.preco)) {
+            msg += 'Informe o preço do produto \n';
+        }
+
+        if(msg != '') {
+            alert(msg);
+            return false
+        }
+        return true;
+    }
+
+    cancelar() {
+        document.getElementById('produto').value = '';
+        document.getElementById('preco').value = '';
+        this.editId = null;
+    }
+
+    deletar(id) {
+        if(confirm('Deseja realmente deletar o produto de id: ' + id)) {
+            let tbody = document.getElementById('tbody');
+
+            for(let i = 0; i < this.arrayProdutos.length; i++) {
+                if(this.arrayProdutos[i].id == id) {
+                    this.arrayProdutos.splice(i, 1);
+                    tbody.deleteRow(i);
+                }
+            }
+        }
+    }
+
+    editar(id) {
+        let produto = this.arrayProdutos.find(prod => prod.id == id);
+        this.editId = id;
+
+        document.getElementById('produto').value = produto.nomeProduto;
+        document.getElementById('preco').value = produto.preco;
+    }
+
+    atualizar(id, produto) {
+        for(let i = 0; i < this.arrayProdutos.length; i++) {
+            if(this.arrayProdutos[i].id == id) {
+                this.arrayProdutos[i].nomeProduto = produto.nomeProduto;
+                this.arrayProdutos[i].preco = produto.preco;
+            }
+        }
+    }
+}
+
+var produto = new Produto();
---
+++ index.js
@@ -0,0 +1,24 @@
+function validateForm(event) {
+    event.preventDefault();
+    const email = document.getElementById('email').value;
+    const password = document.getElementById('password').value;
+    const errorMessage = document.getElementById('error-message');
+
+    if (!email.includes('@')) {
+        errorMessage.textContent = "O email deve conter um '@'";
+        return false;
+    }
+ 
+    const passwordRegex = /^(?=.*[0-9])(?=.*[!@#$%^&*])(?=.{8,})/;
+    if (!passwordRegex.test(password)) {
+        errorMessage.textContent = "A senha deve conter pelo menos um número, um caractere especial e 8 caracteres";
+        return false;
+    }
+
+    errorMessage.textContent = '';
+
+    console.log("Redirecionando para cadastro...");
+    setTimeout(() => window.location.href = "/Cadastro/cadastro.html", 100);
+    return true;
+}
+
